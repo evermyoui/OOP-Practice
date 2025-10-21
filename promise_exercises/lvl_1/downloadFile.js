56e6df8891f5b86f1a6d18afc09b9f2d9dba02e3
said <sexbythewind@gmail.com>
2025-10-21 21:27:00 +0800

    Try nested Promise

--- promise_exercises/lvl_1/downloadFile.js
+++ promise_exercises/lvl_1/downloadFile.js
@@ -1,17 +1,41 @@
-const not2Secs = false;
-const true2Secs = true;
+function checkInternet(){
+    return new Promise((resolve,reject)=>{
+        console.log("Checking Internet...");
+
+        setTimeout(()=>{
+            const randomNum = Math.random();
+            console.log("Internet number: "+ randomNum.toFixed(2));
+            if (randomNum > 0.5){
+                resolve("Internet connected.");
+            }else {
+                reject("No internet connection.");
+            }
+        }, 1000);
+    })
+}
 
 function downloadFile(){
     return new Promise((resolve, reject) =>{
-        if (true2Secs){
-            resolve("File downloaded successfully");
-        }else {
-            reject("Download Failed!");
-        }
-    })
+        setTimeout(()=>{
+            const randomNum = Math.random();
+            console.log("Internet number: "+ randomNum.toFixed(2)); 
+            if (randomNum > 0.3){
+                resolve("Success Download.");
+            }else {
+                reject("Download failed.");
+            }
+        },2000)
+    });
 }
-downloadFile().then((message) => {
-    console.log("Downloading File ... \n" +message);
-}).catch((message) => {
-    console.log("Downloading File ... \n" +message);
-});
+
+checkInternet()
+.then((message)=> {
+    console.log(message);
+    return downloadFile();
+})
+.then((message)=>{
+    console.log(message);
+})
+.catch((error)=>{
+    console.log(error);
+})
