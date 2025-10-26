aa01e4f12e932127160f6d5f18e00d9ebd8ae53c
said <sexbythewind@gmail.com>
2025-10-26 18:47:00 +0800

    fetch API, display posts, add show details functionality

--- promise_exercises/fetch_Practice/display_posts/script.js
+++ promise_exercises/fetch_Practice/display_posts/script.js
@@ -1,63 +1,71 @@
+//importing documents
 const container = document.querySelector(".container");
+const counter = document.querySelector(".counter");
 const prev = document.querySelector(".prev");
-prev.disabled = true;
 const next = document.querySelector(".next");
 
+// variables
 let posts = [];
-let startIndex = 0;
+const startIndex = 0;
+const pageSize = 5;
 
+//fetch function
 async function fetchPosts(){
-    try{
+    try {
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+        //error catching
+        if (!response.ok){
+            throw new Error("Can't Access");
+        }
+        //fetching data
         const data = await response.json();
         posts = data;
-
+        //display 5 posts
         displayPosts();
+        updatePosts();
     }catch(error){
         console.error(error);
-        
-    }
+    } 
 }
-
+//render function
 function displayPosts(){
+    //inializes container
     container.innerHTML = "";
-    const sliced = posts.slice(startIndex, startIndex+5);
+    const sliced = posts.slice(startIndex, startIndex + pageSize);
+    //creating each post
     sliced.forEach(post =>{
         const div = document.createElement("div");
-        div.style.marginBottom = "20px";
+        //div containment
         div.innerHTML = `
             <h3>${post.title}</h3>
-            <p>${post.body}</p>
+            <p class = "paragraph" style = "display: none;">${post.body}</p>
+            <button class = "show-details">Show Details</button>
             <hr>
         `;
+        //importing documents
+        const paragraph = div.querySelector(".paragraph");
+        const btn = div.querySelector(".show-details");
+        // show details button
+        btn.addEventListener("click", ()=>{
+            paragraph.style.display = "block";
+            btn.disabled = true;
+            btn.textContent = `Details shown`;
+        });
+        //add to html
         container.appendChild(div);
-    })
+    });
+    updatePosts();
+}
+//update posts
+function updatePosts(){
+    // button working
+    prev.disabled = startIndex === 0;
+    next.disabled = startIndex + pageSize >= posts.length;
+    
+    // counter
+    const currPage = (startIndex / pageSize) + 1;
+    const lastPage = (posts.length / pageSize);
+    counter.textContent = `Page ${currPage} of ${lastPage}`;
 }
 
-prev.addEventListener("click", ()=>{
-    if(startIndex - 5 >= 0){
-        startIndex-=5;
-        displayPosts();
-    }else{
-        prev.disabled = true;
-    }
-});
-
-next.addEventListener("click", ()=>{
-    if(startIndex + 5 < posts.length){
-        startIndex+=5;
-        prev.disabled = false;
-        displayPosts();
-    }else{
-        next.disabled = true;
-    }
-});
-
-fetchPosts();
-// 0
-// body
-// "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-// id
-// 1
-// title
-// "sunt 
+next.addEventListener()
