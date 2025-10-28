948a2adf34f709344ae883cc5d060789f68cde8b
said <sexbythewind@gmail.com>
2025-10-28 20:00:52 +0800

    Other funcationalities

--- promise_exercises/fetch_Practice/pokemon_api/pokemon_api.js
+++ promise_exercises/fetch_Practice/pokemon_api/pokemon_api.js
@@ -1,49 +1,72 @@
 const container = document.querySelector(".container");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const pageCounter = document.querySelector(".pageCounter");
+
 let startIndex = 0;
-const pokemons = [];
-const pageSize = 10;
+let pokemons = [];
+const TOTAL_POKEMONS = 1010;
+const pageSize = 12;
 
 async function fetchData(){
     try{
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${startIndex}/`);
-        const data = await response.json();
-        pokemons = data;
-
-        displayPokemon();
-
-        for (let i = 30; i<= 41; i++){
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
-        const data = await response.json();
-        pokemons = data
-        const card = document.createElement("div");
-        card.classList.add("card");
-
-        const img = document.createElement("img");
-        img.src = data.sprites.front_default;
-
-        const name = document.createElement("p");
-        name.classList.add("name");
-        name.textContent = data.name.toUpperCase();
-        
-        const type = document.createElement("p");
-        type.classList.add("type");
-        const typeName = data.types[0].type.name;
-        type.textContent = `Type: ${typeName.slice(0,1).toUpperCase() + typeName.slice(1,typeName.length).toLowerCase()}`;
-        
-        card.appendChild(img);
-        card.appendChild(name);
-        card.appendChild(type);
-
-        container.appendChild(card);
+        pokemons = [];
+        for (let i = startIndex + 1; i<= startIndex+ pageSize; i++){
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+            const data = await response.json();
+            pokemons.push(data);
         }
-        // console.log(data.types[0].type.name);
+        displayPokemon();
+        updatePokemons();
     }catch(error){
         console.error(error);
     }
 }
 function displayPokemon(){
-    
+    container.innerHTML = "";
+    pokemons.forEach(pokemon => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const img = document.createElement("img");
+        img.src = pokemon.sprites.front_default;
+
+        const name = document.createElement("p");
+        name.classList.add("name");
+        name.textContent = pokemon.name.toUpperCase();
+        const type = document.createElement("p");
+        type.classList.add("type");
+        const typeName = pokemon.types[0].type.name;
+        type.textContent = `Type: ${typeName.slice(0,1).toUpperCase() + typeName.slice(1,typeName.length).toLowerCase()}`;
+
+        card.appendChild(img);
+        card.appendChild(name);
+        card.appendChild(type);
+
+        container.appendChild(card);
+    })
+}
+function updatePokemons(){
+    prev.disabled = startIndex === 0;
+    next.disabled = startIndex + pageSize >= TOTAL_POKEMONS;
+
+    const currPage = Math.floor(startIndex / pageSize) + 1;
+    const lastPage = Math.ceil(TOTAL_POKEMONS / pageSize);
+
+    pageCounter.textContent = `Page ${currPage} of ${lastPage}`;
 }
 
+prev.addEventListener("click", () =>{
+    if (startIndex - pageSize >= 0){
+        startIndex -= pageSize;
+        fetchData();
+    }
+})
+next.addEventListener("click", () =>{
+    if (startIndex + pageSize < TOTAL_POKEMONS){
+        startIndex+= pageSize;
+        fetchData();
+    }
+})
 
 fetchData();
