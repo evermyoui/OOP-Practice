12b002181cc3b9dfa363dd66582f7fc65f9402ad
said <sexbythewind@gmail.com>
2025-10-31 23:20:55 +0800

    initial css

--- promise_exercises/fetch_Practice/weather_dashboard/script.js
+++ promise_exercises/fetch_Practice/weather_dashboard/script.js
@@ -4,5 +4,6 @@ dotenv.config({ quiet: true });
 const apiKey = process.env.API_KEY;
 
 async function fetchWeather(){
+    
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
 }
