298e4efd4900633b0a65c13e839c7a938e6bc941
said <sexbythewind@gmail.com>
2025-11-01 19:24:59 +0800

    final css

--- promise_exercises/fetch_Practice/weather_dashboard/script.js
+++ promise_exercises/fetch_Practice/weather_dashboard/script.js
@@ -3,7 +3,24 @@ dotenv.config({ quiet: true });
 
 const apiKey = process.env.API_KEY;
 
+// const inputSearch = document.querySelector(".input-search");
+
+
+
+
 async function fetchWeather(){
-    
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    // const input = inputSearch.value.toLowerCase();
+    const input = "athens";
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=metric`);
+        if (!response.ok){
+            throw new Error("Cant access");
+        }
+        const data = await response.json();
+        console.log(data);
+    }catch(error){
+        console.error(error);
+    }
 }
+
+fetchWeather();
