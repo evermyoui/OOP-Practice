53b3ac8015f25328c76e99905c8038b269767a60
said <sexbythewind@gmail.com>
2025-11-28 23:55:29 +0800

    initial connect 4

--- js/factory_functions/3/script.js
+++ js/factory_functions/3/script.js
@@ -4,9 +4,60 @@ function Gameboard(){
     const board = [];
 
     const getBoard = () => board;
+
+    for (let i = 0; i< row; i++){
+        board[i] = [];
+        for (let i = 0; i < column; i++){
+            board[i].push(Cell());
+        }
+    }
+
+    const printBoard = () => {
+        const boardWithCellValue = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardWithCellValue);
+    }
+
+    const dropToken = () => {
+        const availableCell = board.filter((row)=> row[column].getValue() === 0).map(row => row[column]);
+
+        if (!availableCell.length) return;
+    }
 }
 
 function Cell(){
     let value = 0;
-    
+
+    const addToken = (player) => {
+        value = player;
+    }
+
+    const getValue = () => value;
+
+    return {
+        addToken, getValue
+    };
+}
+
+function GameController(playerOne = "Player 1", playerTwo = "Player 2"){
+    const board = Gameboard();
+
+    const players = [{
+        name: playerOne,
+        token: 1
+    },{
+        name: playerTwo,
+        token: 2
+    }];
+
+    let playerActive = players[0];
+
+    const switchPlayerTurn = () => {
+        playerActive = playerActive === players[0] ? players[1] : players[0];
+    }
+
+    const getActivePlayer = () => playerActive;
+
+    const printNewBoard = () => {
+
+    }
 }
