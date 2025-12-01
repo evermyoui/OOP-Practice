345f4a1f59d0fb0c50e922a178b8cf4201f13604
said <sexbythewind@gmail.com>
2025-12-01 21:23:35 +0800

    connect 4

--- js/factory_functions/3/script.js
+++ js/factory_functions/3/script.js
@@ -1,63 +1,93 @@
-function Gameboard(){
-    const row = 6;
-    const column = 7;
-    const board = [];
+class Board {
+    constructor(rows = 6, columns = 7){
+        this.rows = rows;
+        this.columns = columns;
+        this.grid = [];
 
-    const getBoard = () => board;
-
-    for (let i = 0; i< row; i++){
-        board[i] = [];
-        for (let i = 0; i < column; i++){
-            board[i].push(Cell());
+        for (let i = 0; i< rows; i++){
+            const row = [];
+            for (let j = 0; j < columns; j++){
+                row.push(new Cell());
+            }
+            this.grid.push(row);
         }
     }
-
-    const printBoard = () => {
-        const boardWithCellValue = board.map((row) => row.map((cell) => cell.getValue()));
-        console.log(boardWithCellValue);
+    printBoard(){
+        const boardValues = this.grid.map((row)=> row.map((cell)=> cell.getValue()));
+        console.log(boardValues);
     }
-
-    const dropToken = () => {
-        const availableCell = board.filter((row)=> row[column].getValue() === 0).map(row => row[column]);
-
+    dropToken(column, token){
+        const availableCell = this.grid.filter((row)=> row[column].getValue() === 0).map((row) => row[column]);
         if (!availableCell.length) return;
+
+        const lowestRow = availableCell.length -1;
+        this.grid[lowestRow][column].addToken(token);
     }
 }
 
-function Cell(){
-    let value = 0;
-
-    const addToken = (player) => {
-        value = player;
+class Cell {
+    constructor(){
+        this.value = 0;
     }
-
-    const getValue = () => value;
-
-    return {
-        addToken, getValue
-    };
-}
-
-function GameController(playerOne = "Player 1", playerTwo = "Player 2"){
-    const board = Gameboard();
-
-    const players = [{
-        name: playerOne,
-        token: 1
-    },{
-        name: playerTwo,
-        token: 2
-    }];
-
-    let playerActive = players[0];
-
-    const switchPlayerTurn = () => {
-        playerActive = playerActive === players[0] ? players[1] : players[0];
+    addToken(token){
+        this.value = token;
     }
-
-    const getActivePlayer = () => playerActive;
-
-    const printNewBoard = () => {
-
+    getValue(){
+        return this.value;
     }
 }
+
+class Player {
+    constructor(p1 = "playerOne", p2 = "playerTwo"){
+        this.p1 = p1;
+        this.p2 = p2;
+        this.players = [{
+            name: p1,
+            token: 1
+        },{
+            name: p2,
+            token: 2
+        }];
+        this.activePlayer = this.players[0];
+    }
+    switchTurn(){
+        this.activePlayer = this.activePlayer === this.players[0] ? this.players[1] : this.players[0];
+    }
+    getActivePlayer(){
+        return this.activePlayer;
+    }
+}
+
+class Game {
+    constructor(){
+        this.board = new Board();
+        this.player = new Player();
+    }
+    printNewRound(){
+        this.board.printBoard();
+        console.log(`It is ${this.player.getActivePlayer().name}'s turn`);
+    }
+    playRound(column){
+        const currentPlayer = this.player.getActivePlayer();
+        console.log(`Dropping ${currentPlayer.name}'s token into column ${column}`);
+        this.board.dropToken(column, currentPlayer.token);
+
+        this.player.switchTurn();
+        this.printNewRound();
+    }
+    checkWin(activePlayer){
+        const row = this.board.rows;
+        const col = this.board.columns;
+        let count = 1;
+        let goingBackward = false;
+
+        for(let i = 0; i< row; i++){
+
+        }
+    }
+}
+
+const game = new Game();
+game.playRound(3);
+game.playRound(3);
+game.playRound(3);
